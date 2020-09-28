9e431d3d5aeb3aaf7cdea5a69a9b6d6ed68b4a0c
Emma.ou <ouwenxia1994@foxmail.com>
2020-09-29 01:01:47 +0800

    add: Chap10

--- Chap-10 数组和指针/复习题/test.c
+++ Chap-10 数组和指针/复习题/test.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 int main(void)
 {
-  int *ptr;
-  int *ptr2;
-int torf[2][2] = {12, 14, 16};
-int mine[2][2] = {{1,2},{3,4}};
-ptr = torf[0];
-ptr2 = mine[0];
-  printf("%d, %d, %d,%d\n",*ptr, *(ptr+1), *(ptr+2), *(ptr+3));
-  printf("%d, %d, %d\n",*ptr2, *(ptr2+1), *(ptr2+2));
+  int (*ptr)[2];
+  int torf[2][2] = { 12, 14, 16 };
+  ptr = torf;
+  printf("%d, %d\n",**ptr, **(ptr+1));
   return 0;
 }
